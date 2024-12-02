a7498fc330fe42852ca02a5b512bcf33d344f540
irina.ukraintseva <irina.ukraintseva@travelline.ru>
2024-12-02 23:17:28 +0300

    Добавит функцию скрола с начала страницы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import MainPage from '../../pages/main-page/main-page';
 import OfferPage from '../../pages/offer-page/offer-page';
 import LoginPage from '../../pages/login-page/login-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import PrivateRoute from '../private-route/private-route';
+import ScrollToTop from '../scroll-to-top/scroll-to-top';
 import { LoginStatus, RoutePath } from '../../const';
-import { HelmetProvider } from 'react-helmet-async';
 
 type AppProps = {
   cardsNumber: number;
 }
 
 function App({ cardsNumber }: AppProps): JSX.Element {
-  const status = LoginStatus.Auth;
+  const status = LoginStatus.NoAuth;
 
   return (
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path={RoutePath.Index}>
             <Route index element={<MainPage cardsNumber={cardsNumber} loginStatus={status} />}></Route>
@@ -29,7 +31,7 @@ function App({ cardsNumber }: AppProps): JSX.Element {
             }
             />
             <Route path={RoutePath.Offer} element={<OfferPage loginStatus={status} />}></Route>
-            <Route path={RoutePath.NotFound} element={<NotFoundPage loginStatus={status}/>}></Route>
+            <Route path={RoutePath.NotFound} element={<NotFoundPage loginStatus={status} />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
